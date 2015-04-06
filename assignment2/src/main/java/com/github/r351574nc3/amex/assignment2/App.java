5099c23d84aae017249eab962cf7ed86b0bc1247
r351574nc3 <r351574nc3@gmail.com>
2015-04-06 15:50:05 -0700

    Initializing project structure

---
+++ assignment2/src/main/java/com/github/r351574nc3/amex/assignment2/App.java
@@ -0,0 +1,13 @@
+package com.github.r351574nc3.amex.assignment2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
